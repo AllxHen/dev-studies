57cda835e8689d891ae7147e64b84a2ad7fae0ce
Allx <156483803+AllxHen@users.noreply.github.com>
2026-09-16 20:10:20 -0300

    Create 012.js

---
+++ js/elif/012.js
@@ -0,0 +1,12 @@
+let nota = 8
+
+if (nota >= 9){
+    console.log(`Parabéns Nota: A!`)
+}
+else if (nota >= 7){
+    console.log(`Ótimo Nota: B!`)
+}
+else if (nota >= 5){
+    console.log(`Ok, Nota: C`)
+}
+else {console.log(`Mal, nota D`)}
