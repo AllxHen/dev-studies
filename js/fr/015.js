27832bb1111a3f46b8513b93e757cb0cec385a0e
Allx <156483803+AllxHen@users.noreply.github.com>
2026-09-17 21:54:29 -0300

    Create 015.js

---
+++ js/fr/015.js
@@ -0,0 +1,6 @@
+for (let produto = 1; produto <= 10; produto++){
+    let valorProduto = produto * 50
+    if (valorProduto > 200){
+        console.log(`Produto ${produto} Valor: ${valorProduto} Desconto Aplicado!`)
+    }
+}
