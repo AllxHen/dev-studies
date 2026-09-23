1cac56bcd17c39bbb3cc09873bb1425e897b28e4
Allx <156483803+AllxHen@users.noreply.github.com>
2026-09-23 09:59:03 -0300

    Update 020.js

--- js/fr/020.js
+++ js/fr/020.js
@@ -1,6 +1,6 @@
 for (let aluno = 1; aluno <= 20; aluno++){
     let minutos = aluno*15
-    if(minutos >= 180){
+    if(minutos > 180){
         console.log(`Aluno ${aluno} - ${minutos} minutos`)
     }
 }
